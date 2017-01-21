a232789c8ae8054ca97f1643cd10c24bd64cd462
Motoharu Kumanomido <e165720@.u-ryukyu.ac.jp>
2017-01-21 22:33:19 +0900

    second commit

---
+++ src/test/java/jp/ac/uryukyu/ie/e165720/MainTest.java
@@ -0,0 +1,16 @@
+package jp.ac.uryukyu.ie.e165720;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by e165720 on 2017/01/21.
+ */
+public class MainTest {
+    @Test
+    public void main() throws Exception {
+
+    }
+
+}
